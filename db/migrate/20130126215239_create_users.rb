3a4582e583931aa4e9649b38cda6f7f3761c5a7a
Derek Barber <db@derekbarber.ca>
2014-08-06 17:18:32 -0700

    Add name and infusionsoft_affiliate_url to users table

--- db/migrate/20130126215239_create_users.rb
+++ db/migrate/20130126215239_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
       t.string :email
       t.string :referral_code
+      t.string :infusionsoft_affiliate_link
       t.integer :referrer_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -70,11 +70,13 @@ ActiveRecord::Schema.define(:version => 20130312045541) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "name"
     t.string   "email"
     t.string   "referral_code"
+    t.string   "infusionsoft_affiliate_link"
     t.integer  "referrer_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
   end
 
 end
