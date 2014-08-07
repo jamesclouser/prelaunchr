737a5982c9208254071ff69ccd3d21843b6dbe2e
Derek Barber <db@derekbarber.ca>
2014-08-06 17:18:50 -0700

    Add editing capability to ActiveAdmin users table

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,22 @@
 ActiveAdmin.register User do
-  actions :index, :show
+  actions :index, :show, :edit, :update
+  index do
+    column :name
+    column :email
+    column :referral_code
+    column :infusionsoft_affiliate_link
+    column :referrer_id
+    default_actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :name
+      f.input :email
+      f.input :infusionsoft_affiliate_link
+    end
+    f.actions
+  end
 end
