4c085960263b46ce4a66233ec7a3e3a0346cd937
Derek Barber <db@derekbarber.ca>
2014-08-12 10:58:39 -0700

    Set up sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,20 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.default_url_options = { :host => 'www.mynewnormal.me' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'mynewnormal.me',
+    :enable_starttls_auto => true
+  }
+
 end
