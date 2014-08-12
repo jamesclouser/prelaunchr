658b40ef668612b30404a56a7644df50a11673bb
Derek Barber <db@derekbarber.ca>
2014-08-12 15:28:18 -0700

    Fix error in mailer template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => "www.mynewnormal.me" }
+  config.action_mailer.default_url_options = { :host => 'mynewnormal.me' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
