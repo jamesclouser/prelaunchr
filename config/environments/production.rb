2f882f7c148ca4c27cabe4074f2485e109e4a1ae
Derek Barber <db@derekbarber.ca>
2014-08-12 12:10:31 -0700

    Update action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  #config.action_mailer.default_url_options = { :host => "www.mynewnormal.me" }
+  config.action_mailer.default_url_options = { :host => "www.mynewnormal.me" }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
@@ -78,5 +78,5 @@ Prelaunchr::Application.configure do
     :domain         => 'mynewnormal.me',
     :enable_starttls_auto => true
   }
-
+  ActionMailer::Base.delivery_method = :smtp
 end
