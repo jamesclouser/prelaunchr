7a445256e6fe5bf695d50ff3aebb1e9b121a783b
Derek Barber <db@derekbarber.ca>
2014-08-14 09:57:34 -0700

    Update the email template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,10 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'mynewnormal.me' }
+  config.action_mailer.default_url_options = {
+    :host => 'mynewnormal.me',
+    :only_path => false
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
