e18e27486d5732e2ccf201f51ec379d5b170a87e
Derek Barber <db@derekbarber.ca>
2014-08-23 22:49:22 -0700

    Add default value for referrer_id column

--- db/migrate/20130126215239_create_users.rb
+++ db/migrate/20130126215239_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :referral_code
       t.string :infusionsoft_affiliate_link
-      t.integer :referrer_id
+      t.integer :referrer_id, :default => 0
 
       t.timestamps
     end
