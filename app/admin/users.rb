28ded6c63c25a3b0f422f62322bd06e92210a522
Derek Barber <db@derekbarber.ca>
2014-08-25 19:57:51 -0700

    Add CSV import feature

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -19,4 +19,17 @@ ActiveAdmin.register User do
     end
     f.actions
   end
+
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save("user", params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
 end
