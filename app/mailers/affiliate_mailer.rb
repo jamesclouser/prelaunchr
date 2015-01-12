8d6c113ae855c5f1cad715937ab31247545a82df
Derek Barber <db@derekbarber.ca>
2015-01-11 18:53:17 -0800

    Update mailer template text and subject lines

--- app/mailers/affiliate_mailer.rb
+++ app/mailers/affiliate_mailer.rb
@@ -3,7 +3,7 @@ class AffiliateMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "Take Control of Your Health! The Ultimate Healthy Living Bundle is Coming Soon. Tell your friends. Earn Rewards."
+        @twitter_message = "I just rediscovered my inner creative genius. Wanna join me? Plus, earn 1yr of Better Homes and Gardens for FREE."
 
         mail(:to => user.email, :subject => "Welcome Authors and Affiliates!")
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "Take Control of Your Health! The Ultimate Healthy Living Bundle is Coming Soon. Tell your friends and get it free!"
+        @twitter_message = "I just rediscovered my inner creative genius. Wanna join me? Plus, earn 1yr of Better Homes and Gardens for FREE."
 
-        mail(:to => user.email, :subject => "The journey to perfect health starts here – bring your friends!")
+        mail(:to => user.email, :subject => "Get ready to connect with your creative side in 2015 – and bring your friends!")
     end
 end
