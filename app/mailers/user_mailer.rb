6cbb5732357c9056e8b564e98d81096d204096a8
Derek Barber <db@derekbarber.ca>
2015-01-12 21:47:19 -0800

    Update the signup email subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-    default from: "Ultimate Bundles <customerservice@ultimate-bundles.com>"
+    default from: "UltimateBundles <customerservice@ultimate-bundles.com>"
 
     def signup_email(user)
         @user = user
         @twitter_message = "I just rediscovered my inner creative genius. Wanna join me? Plus, earn 1yr of Better Homes and Gardens for FREE."
 
-        mail(:to => user.email, :subject => "Get ready to connect with your creative side in 2015 – and bring your friends!")
+        mail(:to => user.email, :subject => "Your 4-steps mini-course")
     end
 end
