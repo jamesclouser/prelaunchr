b4bb00410b51078e4ebea5fe1bc5fad80ab28801
Derek Barber <db@derekbarber.ca>
2015-04-03 09:15:39 -0700

    New designs for 3 Simple Steps site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,167 +1,164 @@
 class UsersController < ApplicationController
-    before_filter :skip_first_page, :only => :new
+  before_filter :skip_first_page, :only => :new
 
-    def index
-      @users = []
-      User.all.each do |user|
-        if user.referrals.count >= 1 && user.referrals.count < 5
-          @users << user
-        end
+  def index
+    @users = []
+    User.all.each do |user|
+      if user.referrals.count >= 1 && user.referrals.count < 5
+        @users << user
       end
     end
+  end
 
-    def new
-      # remove this later
-      redirect_to "http://ultimate-bundles.com/diy-bundle-2015/"
-      # end of remove this later
+  def new
+    # remove this later
+    #redirect_to "http://ultimate-bundles.com/diy-bundle-2015/"
+    # end of remove this later
 
+    @bodyId = 'home'
+    @is_mobile = mobile_device?
 
-      #@bodyId = 'home'
-      #@is_mobile = mobile_device?
+    @user = User.new
 
-      #@user = User.new
+    @ip_limit = false
 
-      #@ip_limit = false
-
-      #if params.has_key?(:ip_limit)
-      #  @ip_limit = true
-      #end
-
-      #respond_to do |format|
-      #  format.html # new.html.erb
-      #end
+    if params.has_key?(:ip_limit)
+      @ip_limit = true
     end
 
-    def create
-      unless params.has_key?(:user) && params[:user][:email] && params[:user][:name]
-        return redirect_to :action => "new"
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def create
+    unless params.has_key?(:user) && params[:user][:email] && params[:user][:name]
+      return redirect_to :action => "new"
+    end
+
+    # Get user to see if they have already signed up
+    @user = User.find_by_email(params[:user][:email]);
+
+    # If user doesnt exist, make them, and attach referrer
+    if @user.nil?
+
+      cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
+
+      if !cur_ip
+        cur_ip = IpAddress.create(
+          :address => request.env['HTTP_X_FORWARDED_FOR'],
+          :count => 0
+        )
       end
 
-      # Get user to see if they have already signed up
-      @user = User.find_by_email(params[:user][:email]);
+      if cur_ip.count > 2
+        return redirect_to :action => "new", :ip_limit => true
+      else
+        cur_ip.count = cur_ip.count + 1
+        cur_ip.save
+      end
 
-      # If user doesnt exist, make them, and attach referrer
-      if @user.nil?
+      @user = User.new(:email => params[:user][:email], :name => params[:user][:name].titleize)
 
-        cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
+      @referred_by = User.find_by_referral_code(cookies[:h_ref])
 
-        if !cur_ip
-          cur_ip = IpAddress.create(
-            :address => request.env['HTTP_X_FORWARDED_FOR'],
-            :count => 0
-          )
-        end
+      puts '------------'
+      puts @referred_by.email if @referred_by
+      puts params[:user][:email].inspect
+      puts request.env['HTTP_X_FORWARDED_FOR'].inspect
+      puts '------------'
 
-        if cur_ip.count > 2
-          return redirect_to :action => "new", :ip_limit => true
-        else
-          cur_ip.count = cur_ip.count + 1
-          cur_ip.save
-        end
+      if !@referred_by.nil?
+        @user.referrer = @referred_by
+        @user.infusionsoft_affiliate_link = @referred_by.infusionsoft_affiliate_link
+      end
 
-        @user = User.new(:email => params[:user][:email], :name => params[:user][:name].titleize)
+      @user.name = @user.name.titleize
+      @user.save
 
-        @referred_by = User.find_by_referral_code(cookies[:h_ref])
+      if @referred_by
+        @referred_by.delay.infusionsoft_referral
+        #contact = Infusionsoft.contact_find_by_email(@referred_by.email, ['id'])
+        #puts '------------ CALLING INFUSIONSOFT'
+        #puts @referred_by.email
+        #puts contact.inspect
 
-        puts '------------'
-        puts @referred_by.email if @referred_by
-        puts params[:user][:email].inspect
-        puts request.env['HTTP_X_FORWARDED_FOR'].inspect
-        puts '------------'
-
-        if !@referred_by.nil?
-          @user.referrer = @referred_by
-          @user.infusionsoft_affiliate_link = @referred_by.infusionsoft_affiliate_link
-        end
-
-        @user.name = @user.name.titleize
-        @user.save
-
-        if @referred_by
-          @referred_by.delay.infusionsoft_referral
-          #contact = Infusionsoft.contact_find_by_email(@referred_by.email, ['id'])
-          #puts '------------ CALLING INFUSIONSOFT'
-          #puts @referred_by.email
-          #puts contact.inspect
-
-          #if contact.count > 0 && @referred_by.referrals.count == 1
-          #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1216)
-          #elsif contact.count > 0 && @referred_by.referrals.count == 5
-          #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1218)
-          #elsif contact.count > 0 && @referred_by.referrals.count == 10
-          #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1220)
-          #end
-
-          #puts ifs_result
-          #puts '------------'
-        end
-
-        #contact_id = Infusionsoft.contact_add_with_dup_check({:FirstName => @user.name, :Email => @user.email}, 'Email');
-        #if contact_id
-        #  Infusionsoft.contact_add_to_group(contact_id, 1208)
-        #  Infusionsoft.email_optin(@user.email, "RAF App Opt-In")
+        #if contact.count > 0 && @referred_by.referrals.count == 1
+        #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1216)
+        #elsif contact.count > 0 && @referred_by.referrals.count == 5
+        #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1218)
+        #elsif contact.count > 0 && @referred_by.referrals.count == 10
+        #  ifs_result = Infusionsoft.contact_add_to_group(contact[0]["id"], 1220)
         #end
 
-        @user.delay.add_to_infusionsoft
+        #puts ifs_result
+        #puts '------------'
       end
 
-      # Send them over refer action
-      respond_to do |format|
-        if !@user.nil?
-          cookies[:h_email] = { :value => @user.email }
-          #format.html { redirect_to '/refer-a-friend' }
-          unless @user.infusionsoft_affiliate_link.blank?
-            format.html { redirect_to @user.infusionsoft_affiliate_link }
-          else
-            format.html { redirect_to '/refer-a-friend' }
-          end
+      #contact_id = Infusionsoft.contact_add_with_dup_check({:FirstName => @user.name, :Email => @user.email}, 'Email');
+      #if contact_id
+      #  Infusionsoft.contact_add_to_group(contact_id, 1208)
+      #  Infusionsoft.email_optin(@user.email, "RAF App Opt-In")
+      #end
+
+      @user.delay.add_to_infusionsoft
+    end
+
+    # Send them over refer action
+    respond_to do |format|
+      if !@user.nil?
+        cookies[:h_email] = { :value => @user.email }
+        #format.html { redirect_to '/refer-a-friend' }
+        unless @user.infusionsoft_affiliate_link.blank?
+          format.html { redirect_to @user.infusionsoft_affiliate_link }
         else
-          format.html { redirect_to root_path, :alert => "Something went wrong!" }
+          format.html { redirect_to '/refer-a-friend' }
         end
+      else
+        format.html { redirect_to root_path, :alert => "Something went wrong!" }
       end
     end
+  end
 
-    def refer
-        email = cookies[:h_email]
+  def refer
+    email = cookies[:h_email]
 
-        @bodyId = 'refer'
-        @is_mobile = mobile_device?
+    @bodyId = 'refer'
+    @is_mobile = mobile_device?
 
-        @user = User.find_by_email(email)
+    @user = User.find_by_email(email)
 
-        respond_to do |format|
-            if !@user.nil?
-                format.html
-            else
-                format.html { redirect_to root_path, :alert => "Something went wrong!" }
-            end
-        end
+    respond_to do |format|
+      if !@user.nil?
+        format.html
+      else
+        format.html { redirect_to root_path, :alert => "Something went wrong!" }
+      end
     end
+  end
 
-    def policy
+  def policy
 
+  end
+
+  def redirect
+    redirect_to root_path, :status => 404
+  end
+
+  private
+
+  def user_params
+    params.require(:episode).permit(:name, :email, :infusionsoft_affiliate_link)
+  end
+
+  def skip_first_page
+    if !Rails.application.config.ended
+      email = cookies[:h_email]
+      if email and !User.find_by_email(email).nil?
+        redirect_to '/refer-a-friend'
+      else
+        cookies.delete :h_email
+      end
     end
-
-    def redirect
-        redirect_to root_path, :status => 404
-    end
-
-    private
-
-    def user_params
-      params.require(:episode).permit(:name, :email, :infusionsoft_affiliate_link)
-       
-    end
-
-    def skip_first_page
-        if !Rails.application.config.ended
-            email = cookies[:h_email]
-            if email and !User.find_by_email(email).nil?
-                redirect_to '/refer-a-friend'
-            else
-                cookies.delete :h_email
-            end
-        end
-    end
-
+  end
 end
