8513a479c10ddd01a9383f91776f3aa7b477822c
Derek Barber <db@derekbarber.ca>
2015-04-07 22:39:15 -0700

    Incorporate video embed and set up thumbnails for each page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,11 @@
 class VideosController < ApplicationController
-  
+
   def index
     @bodyId = 'videos'
+
+    @video_id = 101
+    if params.key?("video_id")
+      @video_id = params[:video_id].to_i
+    end
   end
 end
