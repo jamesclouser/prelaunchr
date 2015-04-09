3a6027930095be5d8da4b981a91bcf2a7971a697
Derek Barber <db@derekbarber.ca>
2015-04-09 15:42:04 -0700

    Updates to the videos page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,5 +7,8 @@ class VideosController < ApplicationController
     if params.key?("video_id")
       @video_id = params[:video_id].to_i
     end
+
+    email = cookies[:h_email]
+    @user = User.find_by_email(email)
   end
 end
