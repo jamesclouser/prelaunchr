42e90071664a126b6716d1312a58f2885cfc143f
Derek Barber <db@derekbarber.ca>
2015-04-10 15:22:51 -0700

    Update user mailer subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,6 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "I just rediscovered my inner creative genius. Wanna join me? Plus, earn 1yr of Better Homes and Gardens for FREE."
-
-        mail(:to => user.email, :subject => "Your 4-steps mini-course")
+        mail(:to => user.email, :subject => "Your Mini Video-ecourse")
     end
 end
