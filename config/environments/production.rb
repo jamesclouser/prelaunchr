bffebbd73cd4987b5c9e680a06a333b6591a664a
James Clouser <james@jamesclouser.com>
2017-01-29 12:40:54 -0500

    Update host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {
-    :host => 'healthylivingbundle.com',
+    :host => 'assessment.ultimate-bundles.com',
     :only_path => false
   }
 
